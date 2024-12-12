30538fb122339da346538a852f3712fb8c3a6bea
RattanakpanhaKong <krattanakpanha@gmail.com>
2024-12-12 18:59:29 +0700

    2 joysticks and WASD key

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,42 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// [RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
+// public class PlayerController : MonoBehaviour
+// {
+//     [SerializeField] private Rigidbody _rigidbody;         // Reference to the Rigidbody
+//     [SerializeField] private FixedJoystick _leftJoystick; // Joystick for movement
+//     [SerializeField] private FixedJoystick _rightJoystick; // Joystick for rotation
+//     [SerializeField] private Animator _animator;          // Animator for player animations (optional)
+//     [SerializeField] private float _moveSpeed;       // Movement speed
+//     [SerializeField] private float _rotationSpeed; // Rotation speed
+
+//     private void FixedUpdate()
+//     {
+//         // Movement logic using the first joystick
+//         Vector3 movement = new Vector3(_leftJoystick.Horizontal * _moveSpeed, _rigidbody.velocity.y, _leftJoystick.Vertical * _moveSpeed);
+//         _rigidbody.velocity = movement;
+
+//         // Rotation logic using the second joystick
+//         float horizontalRotation = _rightJoystick.Horizontal;
+//         float verticalRotation = _rightJoystick.Vertical;
+
+//         if (horizontalRotation != 0 || verticalRotation != 0)
+//         {
+//             // Calculate the direction to face
+//             Vector3 direction = new Vector3(horizontalRotation, 0, verticalRotation).normalized;
+
+//             // Calculate the target rotation
+//             Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+//             // Smoothly rotate towards the target direction
+//             _rigidbody.rotation = Quaternion.Slerp(_rigidbody.rotation, targetRotation, _rotationSpeed * Time.fixedDeltaTime);
+//         }
+//     }
+// }
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +44,47 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Rigidbody _rigidbody;
-    [SerializeField] private FixedJoystick _joystick;
-    [SerializeField] private Animator _animator;
-    [SerializeField] private float _moveSpeed;
+    [SerializeField] private Rigidbody _rigidbody;         // Reference to the Rigidbody
+    [SerializeField] private FixedJoystick _leftJoystick; // Joystick for movement
+    [SerializeField] private FixedJoystick _rightJoystick; // Joystick for rotation
+    [SerializeField] private Animator _animator;          // Animator for player animations (optional)
+    [SerializeField] private float _moveSpeed = 5f;       // Movement speed
+    [SerializeField] private float _rotationSpeed = 100f; // Rotation speed
 
-    public void FixedUpdate() {
-        _rigidbody.velocity = new Vector3(_joystick.Horizontal * _moveSpeed, _rigidbody.velocity.y, _joystick.Vertical * _moveSpeed);
+    private void FixedUpdate()
+    {
+        // Movement logic (either joystick or WASD keys)
+        Vector3 movement = Vector3.zero;
+
+        // Joystick movement
+        if (_leftJoystick.Horizontal != 0 || _leftJoystick.Vertical != 0)
+        {
+            movement = new Vector3(_leftJoystick.Horizontal * _moveSpeed, _rigidbody.velocity.y, _leftJoystick.Vertical * _moveSpeed);
+        }
+        // WASD movement
+        else
+        {
+            float horizontal = Input.GetAxis("Horizontal"); // A/D or Left/Right Arrow
+            float vertical = Input.GetAxis("Vertical");     // W/S or Up/Down Arrow
+            movement = new Vector3(horizontal * _moveSpeed, _rigidbody.velocity.y, vertical * _moveSpeed);
+        }
+
+        _rigidbody.velocity = movement;
+
+        // Rotation logic using the second joystick
+        float horizontalRotation = _rightJoystick.Horizontal;
+        float verticalRotation = _rightJoystick.Vertical;
+
+        if (horizontalRotation != 0 || verticalRotation != 0)
+        {
+            // Calculate the direction to face
+            Vector3 direction = new Vector3(horizontalRotation, 0, verticalRotation).normalized;
+
+            // Calculate the target rotation
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+            // Smoothly rotate towards the target direction
+            _rigidbody.rotation = Quaternion.Slerp(_rigidbody.rotation, targetRotation, _rotationSpeed * Time.fixedDeltaTime);
+        }
     }
 }
