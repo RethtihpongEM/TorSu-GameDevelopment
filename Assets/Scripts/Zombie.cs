4f1c0c4f0652f9b213adf38a9d436bafcafdf395
RethtihpongEM <rethtihpong@gmail.com>
2024-12-15 13:32:08 +0700

    Follow and trigger attack animation when close to player

---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Pool;
+
+public class Zombie : MonoBehaviour
+{
+  private NavMeshAgent agent;
+  private Transform player;
+  private Animator animator;
+  [SerializeField] float damage = 2f;
+  [SerializeField] float attackDistance = 1.5f;
+
+  private IObjectPool<Zombie> zombiePool;
+
+  public void SetPool(IObjectPool<Zombie> pool)
+  {
+    zombiePool = pool;
+  }
+
+  void Start()
+  {
+    agent = GetComponent<NavMeshAgent>();
+    animator = GetComponent<Animator>();
+    GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+    if (playerObject != null)
+    {
+      player = playerObject.transform;
+    }
+    else
+    {
+      Debug.LogError("Player not found! Make sure the player has the 'Player' tag.");
+    }
+  }
+
+  void Update()
+  {
+    agent.SetDestination(player.position);
+    float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+    if (distanceToPlayer <= attackDistance)
+    {
+      // Stop moving and play attack animation
+      agent.isStopped = true;
+      animator.SetBool("IsAttacking", true);
+    }
+    else
+    {
+      // Move towards the player and play walking animation
+      agent.isStopped = false;
+      agent.SetDestination(player.position);
+
+      animator.SetBool("IsAttacking", false);
+      animator.SetBool("IsWalking", true);
+    }
+  }
+
+
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.CompareTag("Player"))
+    {
+      Player player = other.GetComponent<Player>();
+      player.TakeDamage(damage);
+      Debug.Log("Touch Player");
+      // zombiePool.Release(this);
+    }
+  }
+}
